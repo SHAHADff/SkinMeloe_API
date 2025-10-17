5661a1121e66f3d7ccfcaf84ae412e86f49a1a46
SHAHADff <165215476+SHAHADff@users.noreply.github.com>
2025-10-17 23:13:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ model = tf.keras.models.load_model(MODEL_PATH, compile=False)
 def decode_b64(b64_string):
     """تحويل Base64 إلى صورة PIL"""
     if b64_string.startswith("data:"):
-        b64_string = b64_string.split(",", 1)[1]
+        b64_string = b64_string.split(",", 3)[1]
     image_bytes = base64.b64decode(b64_string)
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")  # 🔥 تأكد من RGB هنا
     return image
@@ -111,3 +111,4 @@ def predict():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
