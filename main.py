76cb694cd13dc07752a9a2de0af1efb1da3158b2
SHAHADff <165215476+SHAHADff@users.noreply.github.com>
2025-10-17 23:01:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -88,5 +88,7 @@ def predict():
 # لتشغيل محلي أو على Render
 # -------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
+    port = int(os.environ.get("PORT", 8080))  # Render يستخدم هذا المنفذ
+    app.run(host="0.0.0.0", port=port)
+
 
